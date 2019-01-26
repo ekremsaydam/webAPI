ec53af6788397dd409628d7da592f99f382bc64c
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-26 15:03:15 +0300

    AutoMapper

--- SehirRehberi.API/SehirRehberi.API/Startup.cs
+++ SehirRehberi.API/SehirRehberi.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,7 @@ namespace SehirRehberi.API
         {
             services.AddDbContext<DataContext>(x =>
                 x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAutoMapper();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
             services.AddScoped<IAppRepository, AppRepository>();
