b39317d4f3a7f16d67743677eb37bcc74bb53af5
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2019-01-26 14:31:13 +0300

    IAppRepository

--- SehirRehberi.API/SehirRehberi.API/Startup.cs
+++ SehirRehberi.API/SehirRehberi.API/Startup.cs
@@ -30,6 +30,7 @@ namespace SehirRehberi.API
                 x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddScoped<IAppRepository, AppRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
